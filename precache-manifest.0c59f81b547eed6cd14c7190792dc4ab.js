2c48500253f17223f556ba680f75c65af7d220e8
matheus <matheus.xmaz10@gmail.com>
2021-03-09 16:04:18 -0300

    Updates

---
+++ precache-manifest.0c59f81b547eed6cd14c7190792dc4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be3dbc41eb75329c36131838491ade3",
+    "url": "/Personal-Portifolio/index.html"
+  },
+  {
+    "revision": "c1ccb277212a5803efbe",
+    "url": "/Personal-Portifolio/static/css/main.0f098d0f.chunk.css"
+  },
+  {
+    "revision": "049086e53a616c3a172a",
+    "url": "/Personal-Portifolio/static/js/2.c26d0144.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Personal-Portifolio/static/js/2.c26d0144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ccb277212a5803efbe",
+    "url": "/Personal-Portifolio/static/js/main.319c789f.chunk.js"
+  },
+  {
+    "revision": "f32c49cffdb7dda26f3b",
+    "url": "/Personal-Portifolio/static/js/runtime-main.71304501.js"
+  }
+]);
